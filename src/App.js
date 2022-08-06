2b315ef7c499b48526d7827f5cba06f30e437511
robust-enigma <shubhankitchowdhury0@gmail.com>
2022-08-06 10:47:45 +0530

    animax

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import MainContent from "./components/MainContent";
+
+function App() {
+  //using react hooks
+  const [animeList, SetAnimeList] = useState([]);
+  const [topAnime, SetTopAnime] = useState([]);
+  const [search, SetSearch] = useState("");
+
+  const GetTopAnime = async () => {
+    const temp = await fetch(
+      `https://api.jikan.moe/v3/top/anime/1/bypopularity`
+    ).then((res) => res.json());
+
+    SetTopAnime(temp.top.slice(0, 10)); //top 10 out of 50
+  };
+
+  const HandleSearch = (e) => {
+    //form submission
+    e.preventDefault();
+
+    FetchAnime(search);
+  };
+
+  const FetchAnime = async (query) => {
+    //to find a particular anime
+    const temp = await fetch(
+      `https://api.jikan.moe/v3/search/anime?q=${query}&order_by=title&sort=asc&limit=10`
+    ).then((res) => res.json());
+
+    SetAnimeList(temp.results); //getting back results
+  };
+
+  useEffect(() => {
+    GetTopAnime();
+  }, []);
+
+  return (
+    <div className="App">
+      <Header />
+      <div className="content-wrap">
+        <Sidebar topAnime={topAnime} />
+        <MainContent
+          HandleSearch={HandleSearch}
+          search={search}
+          SetSearch={SetSearch}
+          animeList={animeList}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
